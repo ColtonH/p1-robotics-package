ff5687ffa140de424c5f7e0da09a1d598fcf3ced
Colton Hurt <hurt0504@gpel3.cs.ou.edu>
2014-11-21 14:05:42 -0600

    Had to remake project

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/course/hurt0504/robotics_projects_ws/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/opt/ros/hydro".split(';') if "/opt/ros/hydro" != "" else []
+underlay_workspaces = "/home/course/hurt0504/robotics_projects_ws/devel;/opt/ros/hydro".split(';') if "/home/course/hurt0504/robotics_projects_ws/devel;/opt/ros/hydro" != "" else []
